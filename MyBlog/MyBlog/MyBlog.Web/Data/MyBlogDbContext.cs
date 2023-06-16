7a83ace7c132a6a925a09f0bd57e71d981f36fc3
durajlinda <lindaduraj9@gmail.com>
2023-06-16 16:21:38 +0200

    first commit

---
+++ MyBlog/MyBlog/MyBlog.Web/Data/MyBlogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MyBlog.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyBlog.Web.Data
+{
+    public class MyBlogDbContext : DbContext
+
+
+    {
+        public MyBlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPost{ get; set; }
+        public DbSet<Tag> Tag { get; set; }
+    }
+}
